aa83fc43e8716e19396e0311884528885af9cbdb
GiovaneWerlang <giovanewerlang@alunos.utfpr.edu.br>
2025-02-22 20:29:29 -0300

    base task

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,8 +1,8 @@
 import { Component, Input } from '@angular/core';
-
+import { TaskComponent } from './task/task.component';
 @Component({
   selector: 'app-tasks',
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })

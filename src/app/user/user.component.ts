e6519521cf50bcf13ea7d4a53864b4ddffa3a95d
GiovaneWerlang <giovanewerlang@alunos.utfpr.edu.br>
2025-02-22 19:36:39 -0300

    inputs

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,4 @@
-import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
-
-import { DUMMY_USERS } from '../dummy-users';
-import { GenerateRandomIndex } from '../shared/utils/randomindex';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -11,11 +8,15 @@ import { GenerateRandomIndex } from '../shared/utils/randomindex';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UserComponent {
-  //Angular detects the changes to the signal and what is being computed to re-render the component.
-  selectedUser = signal(DUMMY_USERS[GenerateRandomIndex(DUMMY_USERS?.length ?? 0)]);
-  pathImage = computed(() => 'assets/users/' + this.selectedUser().avatar);
 
+  @Input('avatar') avatar!:string;
+  @Input('name') name!:string;
+
+  get pathImage(){
+    return 'assets/users/' + this.avatar;
+  }
+ 
   onSelectedUser(){
-    this.selectedUser.set(DUMMY_USERS[GenerateRandomIndex(DUMMY_USERS?.length ?? 0)]);
+
   }
 }

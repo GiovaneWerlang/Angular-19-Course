ac351e9b7c6d28d9338408705cfe16e815921d3b
GiovaneWerlang <giovanewerlang@alunos.utfpr.edu.br>
2025-03-23 17:32:36 -0300

    template loops

--- src/app/dashboard/tickets/tickets.component.ts
+++ src/app/dashboard/tickets/tickets.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { NewTicketComponent } from "./new-ticket/new-ticket.component";
 import { Ticket } from './models/ticket.model';
 import { TicketData } from './models/ticketdata.model';
+import { TicketComponent } from "./ticket/ticket.component";
 
 @Component({
   selector: 'app-tickets',
-  imports: [NewTicketComponent],
+  imports: [NewTicketComponent, TicketComponent],
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.css'
 })

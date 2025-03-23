4ec9a11e49308fdee27f39978ecfd1ff678b3152
GiovaneWerlang <giovanewerlang@alunos.utfpr.edu.br>
2025-03-23 12:14:35 -0300

    host elements

--- src/app/dashboard/dashboard-item/dashboard-item.component.ts
+++ src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -4,7 +4,7 @@ import { Component, input, Input } from '@angular/core';
   selector: 'app-dashboard-item',
   imports: [],
   templateUrl: './dashboard-item.component.html',
-  styleUrl: './dashboard-item.component.css'
+  styleUrl: './dashboard-item.component.css',
 })
 export class DashboardItemComponent {
   // @Input({required: true}) image!: { src: string, alt: string};
--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ControlComponent } from '../../../shared/control/control.component';
+import { ButtonComponent } from '../../../shared/button/button.component';
 
 @Component({
   selector: 'app-new-ticket',
-  imports: [ControlComponent],
+  imports: [ControlComponent, ButtonComponent],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })

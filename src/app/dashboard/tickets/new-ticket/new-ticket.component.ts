24980ea96b0573afda0925ea851617ddca229750
GiovaneWerlang <giovanewerlang@alunos.utfpr.edu.br>
2025-03-23 17:26:54 -0300

    output

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,7 +1,8 @@
-import { afterNextRender, afterRender, AfterViewInit, Component, ElementRef, OnInit, viewChild, ViewChild } from '@angular/core';
+import { afterNextRender, afterRender, AfterViewInit, Component, ElementRef, EventEmitter, OnInit, output, viewChild, ViewChild } from '@angular/core';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { FormsModule } from '@angular/forms';
+import { TicketData } from '../models/ticketdata.model';
 
 @Component({
   selector: 'app-new-ticket',
@@ -12,6 +13,8 @@ import { FormsModule } from '@angular/forms';
 export class NewTicketComponent implements OnInit, AfterViewInit {
   // @ViewChild('form') form?: ElementRef<HTMLFormElement>;
   private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+  // @Output() add = new EventEmitter();
+  add = output<TicketData>();
 
   constructor(){
     afterRender(() => {
@@ -26,6 +29,7 @@ export class NewTicketComponent implements OnInit, AfterViewInit {
   onSubmit(title: string, ticketText: string) {
     console.log(title)
     console.log(ticketText)
+    this.add.emit({title: title, text: ticketText});
     this.form().nativeElement.reset();
   }
 
--- src/app/dashboard/tickets/tickets.component.ts
+++ src/app/dashboard/tickets/tickets.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NewTicketComponent } from "./new-ticket/new-ticket.component";
 import { Ticket } from './models/ticket.model';
+import { TicketData } from './models/ticketdata.model';
 
 @Component({
   selector: 'app-tickets',
@@ -10,4 +11,15 @@ import { Ticket } from './models/ticket.model';
 })
 export class TicketsComponent {
   tickets:Ticket[] = [];
+
+  onAdd(ticketData: TicketData){
+    const ticket: Ticket = {
+      title: ticketData.title,
+      request: ticketData.text,
+      id: Math.random().toString(),
+      status: 'open'
+    };
+    this.tickets.push(ticket);
+  }
+
 }

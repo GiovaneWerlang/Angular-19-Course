587e53e4382bb00094ccfd8921d48ca8184f98bc
GiovaneWerlang <giovanewerlang@alunos.utfpr.edu.br>
2025-03-30 13:02:35 -0300

    directive change behaviour

--- src/app/safe-link.directive.ts
+++ src/app/safe-link.directive.ts
@@ -2,11 +2,23 @@ import { Directive } from "@angular/core";
 
 @Directive({
     selector: 'a[appSafeLink]',
-
+    host: {
+        '(click)': 'onConfirmLeavePage($event)'
+    }
 })
 export class SafeLinkDirective {
     constructor() {
         console.log('directive in use')
+    }
+
+    onConfirmLeavePage(event: MouseEvent) {
+        const leave = window.confirm('Do you want to leave?');
+
+        if(leave) {
+            return;
+        }
+
+        event?.preventDefault();
     }
 
 }

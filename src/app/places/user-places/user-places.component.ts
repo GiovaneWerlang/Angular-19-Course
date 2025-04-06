d3cd7c3cbc33006fdafc18b55c8343e57f145210
GiovaneWerlang <giovanewerlang@alunos.utfpr.edu.br>
2025-04-06 18:36:47 -0300

    delete

--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -1,10 +1,8 @@
+import { Place } from './../place.model';
 import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
-import { HttpClient } from '@angular/common/http';
-import { map, catchError, throwError } from 'rxjs';
-import { Place } from '../place.model';
 import { PlacesService } from '../places.service';
 
 @Component({
@@ -35,6 +33,15 @@ export class UserPlacesComponent implements OnInit {
 
     this.destroyRef.onDestroy(() => {
       subscription.unsubscribe();
-    })
+    });
   }
+
+  onRemovePlace(place: Place){
+    const subscription = this.placesService.removeUserPlace(place).subscribe();
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
+
 }

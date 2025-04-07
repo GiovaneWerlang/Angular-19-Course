8d26726ed614e949bea3f4f16ecf740ce2d111f3
GiovaneWerlang <giovanewerlang@alunos.utfpr.edu.br>
2025-04-07 20:48:43 -0300

    validate form with directives

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -11,6 +11,9 @@ import { FormsModule, NgForm } from '@angular/forms';
 export class LoginComponent {
 
   onSubmit(formData: NgForm) {
+    if(formData.form.invalid){
+      return;
+    }
     const email = formData.form.value.email;
     const password = formData.form.value.password;
     console.log(email, password)

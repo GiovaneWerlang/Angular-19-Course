fe09631ddbe95ee722df418b127a799b8af0f4f7
GiovaneWerlang <giovanewerlang@alunos.utfpr.edu.br>
2025-04-12 17:29:21 -0300

    multi input validation

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,44 +1,62 @@
 import { Component } from '@angular/core';
-import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+function equalValues(controlName1: string, controlName2: string) {
+  return (control: AbstractControl) => {
+    const value1 = control.get(controlName1)?.value;
+    const value2 = control.get(controlName2)?.value;
+
+    if (value1 === value2) {
+      return null;
+    }
+
+    return { valuesNotEqual: true };
+  }
+}
 
 @Component({
   selector: 'app-signup',
   standalone: true,
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
-  imports: [ ReactiveFormsModule]
+  imports: [ReactiveFormsModule]
 })
 export class SignupComponent {
 
   form = new FormGroup({
-    email: new FormControl('', { validators: [Validators.email, Validators.required]}),
+    email: new FormControl('', { validators: [Validators.email, Validators.required] }),
     passwords: new FormGroup({
-      password: new FormControl('', { validators: [Validators.minLength(6), Validators.required]}),
-      confirmPassword: new FormControl('', { validators: [Validators.minLength(6), Validators.required]}),  
+      password: new FormControl('', { validators: [Validators.minLength(6), Validators.required] }),
+      confirmPassword: new FormControl('', { validators: [Validators.minLength(6), Validators.required] }),
+    }, {
+      validators: [equalValues('password', 'confirmPassword')]
     }),
-    firstName: new FormControl('', { validators: [Validators.required]}),
-    lastName: new FormControl('', { validators: [Validators.required]}),
+    firstName: new FormControl('', { validators: [Validators.required] }),
+    lastName: new FormControl('', { validators: [Validators.required] }),
     address: new FormGroup({
-      street: new FormControl('', { validators: [Validators.required]}),
-      number: new FormControl('', { validators: [Validators.required]}),
-      postalCode: new FormControl('', { validators: [Validators.required]}),
-      city: new FormControl('', { validators: [Validators.required]}),
-    }),    
-    role: new FormControl<'student'|'teacher'|'employee'|'founder'|'other'>('other', { validators: [Validators.required]}),
+      street: new FormControl('', { validators: [Validators.required] }),
+      number: new FormControl('', { validators: [Validators.required] }),
+      postalCode: new FormControl('', { validators: [Validators.required] }),
+      city: new FormControl('', { validators: [Validators.required] }),
+    }),
+    role: new FormControl<'student' | 'teacher' | 'employee' | 'founder' | 'other'>('other', { validators: [Validators.required] }),
     source: new FormArray([
       new FormControl(false),
       new FormControl(false),
       new FormControl(false),
     ]),
-    agree: new FormControl(false, { validators: [Validators.required]}),
+    agree: new FormControl(false, { validators: [Validators.required] }),
   });
 
-  onSubmit(){
+  onSubmit() {
+    if (this.form.invalid) {
+      return;
+    }
     const formValues = this.form.getRawValue();
     console.log(formValues);
   }
 
-  onReset(){
+  onReset() {
     this.form.reset();
   }
 }

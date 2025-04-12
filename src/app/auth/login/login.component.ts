c4e1097f8707635f6846ece2ee1963b0f5bfd1cf
GiovaneWerlang <giovanewerlang@alunos.utfpr.edu.br>
2025-04-12 16:13:36 -0300

    underlying form object

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { afterNextRender, Component, DestroyRef, inject, viewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -10,6 +11,19 @@ import { FormsModule, NgForm } from '@angular/forms';
 })
 export class LoginComponent {
 
+  private form = viewChild<NgForm>('form');
+  private destroyRef = inject(DestroyRef);
+
+  constructor(){
+    afterNextRender(() => {
+      const subscription = this.form()?.valueChanges?.pipe(debounceTime(500))
+      .subscribe({
+        next: (value) => window.localStorage.setItem('login-email', JSON.stringify({ email: value.email}))
+      });
+      this.destroyRef.onDestroy(() => subscription?.unsubscribe());
+    });
+  }
+
   onSubmit(formData: NgForm) {
     if(formData.form.invalid){
       return;
@@ -17,6 +31,8 @@ export class LoginComponent {
     const email = formData.form.value.email;
     const password = formData.form.value.password;
     console.log(email, password)
+
+    formData.form.reset();
   }
 
 }
